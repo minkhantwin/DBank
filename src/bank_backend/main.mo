a54da9867bbdf00944075c9700b4c49f3dac9474
Min Khant <alexandarminkhant@MacBook-Pro-2.local>
2022-11-27 22:23:37 +0630

    dapp

--- src/bank_backend/main.mo
+++ src/bank_backend/main.mo
@@ -1,5 +1,56 @@
-actor {
-  public query func greet(name : Text) : async Text {
-    return "Hello, " # name # "!";
-  };
-};
+import Debug "mo:base/Debug";
+import Nat "mo:base/Nat";
+import Time "mo:base/Time";
+import Float "mo:base/Float";
+
+actor DBank{
+	var currentValue: Float = 300;
+	currentValue := 300; 
+	stable var startTime = Time.now();
+	// startTime := Time.now();
+
+	let id = 236565656;
+
+	// Debug.print(debug_show(currentValue));
+
+	public func topUp(amount: Float)
+	{
+		currentValue := currentValue + amount;
+		Debug.print(debug_show(currentValue));
+	};
+ 
+	public func withdraw(amount: Float)
+	{
+		let temp: Float = currentValue - amount;
+		if (temp >= 0)
+		{
+			currentValue := currentValue - amount;
+			Debug.print(debug_show(currentValue));
+		}
+		else 
+		{
+			Debug.print("Amount too large. currentValue will less than zero!")
+		}
+	};
+
+	public query func checkBalance(): async Float
+	{
+		return currentValue;
+	};
+
+	public func compound()
+	{
+		let currentTime = Time.now();
+		var timeCollapse = currentTime - startTime;
+		timeCollapse := timeCollapse / 1000000000;
+		currentValue := currentValue * (1.01 ** Float.fromInt(timeCollapse));
+
+		startTime := currentTime;
+	};
+
+}
+
+
+
+
+
--- src/bank_frontend/src/index.js
+++ src/bank_frontend/src/index.js
@@ -1,19 +1,29 @@
-import { bank_backend } from "../../declarations/bank_backend";
+import { bank_backend as dbank } from "../../declarations/bank_backend/index";
+window.addEventListener("load", async function () {
+	// console.log("Finished loading")
+	const currentAmount = await dbank.checkBalance();
+	this.document.getElementById("value").innerText =
+		Math.round(currentAmount * 100) / 100;
+});
+ 
+document.querySelector("form").addEventListener("submit", async function (e) {
+	e.preventDefault();
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
+	const button = e.target.querySelector('#submit-btn');
+	const inputElement = document.getElementById('input-amount');
+	const outputElement = document.getElementById('withdrawal-amount')
+	const inputAmount = parseFloat(inputElement.value);
+	const outputAmount = parseFloat(outputElement.value);
+	
+	button.setAttribute('disabled', true);
+	
+	await dbank.topUp(inputAmount || 0);
+	await dbank.withdraw(outputAmount|| 0);
 
-  const name = document.getElementById("name").value.toString();
-
-  button.setAttribute("disabled", true);
-
-  // Interact with foo actor, calling the greet method
-  const greeting = await bank_backend.greet(name);
-
-  button.removeAttribute("disabled");
-
-  document.getElementById("greeting").innerText = greeting;
-
-  return false;
+	
+	const currentAmount = await dbank.checkBalance();
+	document.getElementById("value").innerText = Math.round(currentAmount * 100) / 100;
+	button.removeAttribute('disabled');
+	inputElement.value = '';
+	outputElement.value = '';
 });
